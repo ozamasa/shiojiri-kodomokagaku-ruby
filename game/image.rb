da27290a70960d1cfe8e49250221a68508641e02
Masaki Ozawa <ozamasa123@gmail.com>
2013-09-03 15:03:23 +0900

    Refactoring

--- game/image.rb
+++ game/image.rb
@@ -1,25 +1,120 @@
 # encoding: utf-8
 
-require 'sdl'
-
 class Image
 
-  attr_accessor :x, :y, :speed, :image
+  attr_accessor :x, :y, :w, :h, :speed, :image
 
-  def initialize(screen, image_file, x, y, s)
+  def initialize(screen, image_file, x, y, w, h)
     @screen = screen
     @image = SDL::Surface.load(image_file)
-    reset(x, y, s)
-  end
+    @w = w
+    @h = h
 
-  def reset(x, y, s)
     @x = x
     @y = y
-    @speed = s + 0.1
+    @speed = 0
   end
 
   def put(*map)
     @screen.put(@image, @x, @y)
+  end
+
+  def hit(target, range)
+    source = self
+    return false if target.y <= 0 || source.y <= 0
+
+    if (target.y - (source.y + source.h) < 0) and
+       ((source.x + source.w / 2) - (target.x + target.w / 2)) > range * -1 and
+       ((source.x + source.w / 2) - (target.x + target.w / 2)) < range
+      return true
+    end
+    return false
+  end
+
+end
+
+
+class Enemy < Image
+
+  attr_accessor :ufo
+
+  def initialize(screen, x, ufo)
+    image_file = 'image/enemy.png'
+    w = 37
+    h = 50
+    super(screen, image_file, ufo.x + x, ufo.y + 80, w, h)
+    @ufo = ufo
+  end
+
+  def reset(xx)
+    @x = @ufo.x + xx
+    @y = @ufo.y + 80
+    @speed = 0.3
+  end
+
+end
+
+
+class Ufo < Image
+
+  def initialize(screen)
+    image_file = 'image/ufo.png'
+    x = 50
+    y = 10
+    w = 200
+    h = 120
+    super(screen, image_file, x, y, w, h)
+  end
+
+end
+
+
+class Bom < Image
+
+  def initialize(screen)
+    image_file = 'image/bomb.png'
+    x = -50
+    y = -50
+    w = 41
+    h = 51
+    super(screen, image_file, x, y, w, h)
+  end
+
+  def launch(gun)
+    if @y <= 0
+      @x = gun.x + 30
+      @y = gun.y
+      @speed = 0.5
+    end
+  end
+
+end
+
+
+class Gun < Image
+
+  def initialize(screen)
+    image_file = 'image/gun.png'
+    x = SCREEN_W - 80
+    y = BOTTOM - 115
+    w = 97
+    h = 119
+    super(screen, image_file, x, y, w, h)
+  end
+
+end
+
+
+class Ban < Image
+
+  def initialize(screen)
+    image_file = 'image/bang.png'
+    x = -50
+    y = -50
+    w = 58
+    h = 72
+    super(screen, image_file, x, y, w, h)
+#   @image.set_color_key(SDL::SRCCOLORKEY, [255, 255, 255])
   end
 
 end
--- game/main.rb
+++ game/main.rb
@@ -7,19 +7,23 @@ SCREEN_W = 640
 SCREEN_H = 480
 BOTTOM   = 410
 
-ufo_hp = SCREEN_W - 100
-you_hp = SCREEN_W - 100
-
 SDL.init(SDL::INIT_EVERYTHING)
 SDL::TTF.init
 screen = SDL.set_video_mode(SCREEN_W, SCREEN_H, 16, SDL::SWSURFACE)
-font = SDL::TTF.open('font/ehsmb.ttf', 20)
 
-bom = Image.new(screen, 'image/bomb.png', -50, -50, 0)
-ufo = Image.new(screen, 'image/ufo.png', 50, 10, 0)
-en1 = Image.new(screen, 'image/enemy.png', ufo.x + 30, ufo.y + 80, 1 / 3.0)
-en2 = Image.new(screen, 'image/enemy.png', ufo.x + 85, ufo.y + 80, 2 / 3.0)
-gun = Image.new(screen, 'image/gun.png', SCREEN_W - 80, BOTTOM - 115, 0)
+bom = Bom.new(screen)
+gun = Gun.new(screen)
+ufo = Ufo.new(screen)
+
+en1 = Enemy.new(screen, 30,  ufo)
+en2 = Enemy.new(screen, 140, ufo)
+en1.speed = 0.3
+en2.speed = 0.5
+
+ufo_hp = SCREEN_W - 100
+you_hp = SCREEN_W - 100
+
+font = SDL::TTF.open('font/ehsmb.ttf', 20)
 
 loop do
   while event=SDL::Event2.poll
@@ -27,10 +31,11 @@ loop do
     when SDL::Event2::Quit
       exit
     when SDL::Event2::KeyDown
-      sleep(10) if event.sym == SDL::Key::ESCAPE
+      exit if event.sym == SDL::Key::ESCAPE
     end
   end
 
+  # 大砲を動かす
   SDL::Key.scan
   if SDL::Key.press?(SDL::Key::LEFT)
     gun.x -= 2
@@ -38,65 +43,80 @@ loop do
   if SDL::Key.press?(SDL::Key::RIGHT)
     gun.x += 2
   end
-  if SDL::Key.press?(SDL::Key::SPACE)
-    if bom.y < 0
-      bom.reset(gun.x + 30, gun.y, 0.5)
-    end
-  end
-
-  if bom.y < 0
-    bom.reset(-50, -50, 0)
-  end
-  bom.y -= bom.speed
 
   if gun.x < 0
     gun.x = 0
   end
-  if gun.x >= SCREEN_W-100
-    gun.x = SCREEN_W-100
+  if gun.x >= SCREEN_W - 100
+    gun.x = SCREEN_W - 100
   end
 
-  if en1.y > BOTTOM
-    ufo.x = rand(SCREEN_W - 150)
-    en1.reset(ufo.x + 30, ufo.y + 80, 1 / 3.0)
+
+  # ミサイルを発射させる
+  if SDL::Key.press?(SDL::Key::SPACE)
+    bom.launch(gun)
+    bom.speed = 0.5
+  end
+
+  if bom.y > 0
+    bom.y -= bom.speed
+  end
+
+
+  # エイリアン１を落とす
+  if en1.y > BOTTOM - en1.h
+    ufo.x = rand(SCREEN_W - ufo.w)
+    en1.reset(30)
+    en1.speed = 0.3
   end
   en1.y += en1.speed
 
-  if en2.y > BOTTOM
-    ufo.x = rand(SCREEN_W - 150)
-    en2.reset(ufo.x + 85, ufo.y + 80, rand(3) / 3.0)
+  # エイリアン２を落とす
+  if en2.y > BOTTOM - en2.h
+    ufo.x = rand(SCREEN_W - ufo.w)
+    en2.reset(140)
+    en2.speed = 0.5
   end
   en2.y += en2.speed
 
-  if bom.y > 0 and (bom.y - ufo.y - 80) < 0 and (ufo.x + 100 - bom.x - 10) > -90 and (ufo.x + 100 - bom.x - 10) < 90
+
+  # ＵＦＯがミサイルに当たったか
+  if ufo.hit(bom, 80)
     ufo_hp -= 30
-    bom.reset(-50, -50, 0)
+    bom.y = -50
   end
   break if ufo_hp <= 0
 
-  if (gun.y - en1.y - 50) < 0 and (en1.x + 10 - gun.x - 40) > -30 and (en1.x + 10 - gun.x - 40) < 30
+  # エイリアン１が大砲に当たったか
+  if en1.hit(gun, 30)
     you_hp -= 40
-    ufo.x = rand(SCREEN_W - 150)
-    en1.reset(ufo.x + 30, ufo.y + 80, 1 / 3.0)
+    ufo.x = rand(SCREEN_W - ufo.w)
+    en1.reset(30)
+    en1.speed = 0.3
   end
   break if you_hp <= 0
 
-  if (gun.y - en2.y - 50) < 0 and (en2.x + 10 - gun.x - 40) > -30 and (en2.x + 10 - gun.x - 40) < 30
+  # エイリアン２が大砲に当たったか
+  if en2.hit(gun, 30)
     you_hp -= 80
-    ufo.x = rand(SCREEN_W - 150)
-    en2.reset(ufo.x + 30, ufo.y + 80, 1 / 3.0)
+    ufo.x = rand(SCREEN_W - ufo.w)
+    en2.reset(140)
+    en2.speed = 0.5
   end
   break if you_hp <= 0
 
+
+  # ゲームウィンドウを表示する
   screen.fill_rect(0, 0,      SCREEN_W, BOTTOM,          [46, 41, 48])
   screen.fill_rect(0, BOTTOM, SCREEN_W, SCREEN_H-BOTTOM, [0,  0,  0 ])
 
+  # ＨＰを表示する
   screen.fill_rect(10, BOTTOM + 10, ufo_hp, 20, [255, 0, 0])
   screen.fill_rect(10, BOTTOM + 40, you_hp, 20, [255, 255, 0])
-
   font.draw_solid_utf8(screen, "#{ufo_hp}", SCREEN_W - 80, BOTTOM + 10, 255, 255, 255)
   font.draw_solid_utf8(screen, "#{you_hp}", SCREEN_W - 80, BOTTOM + 40, 255, 255, 255)
 
+  # キャラクターを表示する
   bom.put if bom.y > 0
   gun.put
   ufo.put
@@ -104,18 +124,20 @@ loop do
   en2.put
 
   screen.update_rect(0, 0, 0, 0)
-
 end
 
+
+# ゲームオーバーを表示する
 font = SDL::TTF.open('font/ehsmb.ttf', 50)
 if you_hp <= 0
-  screen.fill_rect(0, 0, SCREEN_W, SCREEN_H,  [255, 0, 0])
+  screen.fill_rect(0, 0, SCREEN_W, SCREEN_H, [255, 0, 0])
   font.draw_solid_utf8(screen, "GAME OVER...", 150, 200, 0, 0, 0)
 elsif ufo_hp <= 0
-  screen.fill_rect(0, 0, SCREEN_W, SCREEN_H,  [255, 255,   0])
+  screen.fill_rect(0, 0, SCREEN_W, SCREEN_H, [255, 255, 0])
   font.draw_solid_utf8(screen, "MISSION CLEAR!!", 100, 200, 0, 0, 0)
 end
 screen.update_rect(0, 0, 0, 0)
+
 
 sleep(3)
 exit
